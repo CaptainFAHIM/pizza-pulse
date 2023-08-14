60dd5c08c7b72b9af9d090eec03939eb90b5c1dc
CaptainFAHIM <mehedihfahim12@gmail.com>
2023-08-14 13:59:33 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// External imports
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+require("dotenv").config();
+const path = require("path");
+
+const PORT = process.env.PORT || 3300;
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+// Set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
